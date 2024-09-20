1af0d80c40f78c5842d27bd1238f2ec7388f748a
rogcomfox <joeg.11021996@gmail.com>
2024-09-20 14:55:58 +0700

    fix: remove multi regression

--- section3/regression.py
+++ section3/regression.py
@@ -24,38 +24,6 @@ class SimpleLinearRegression():
     def predict(self, x_test: np.ndarray):
         return self.intercept + self.slope * x_test
 
-class MultiLinearRegression():
-    '''
-        Multi Linear Regression class. This algorithm has an equation as follow -> y = β0 + β1xi1 + ... + βpxip
-
-        y = dependent variable; β0 = intercept; βi = slope; xi = features variable
-
-        Input: learning rate of model, error rate threshold
-    '''
-    def __init__(self, learning_rate: float = 0.00001, epsilon: float = 0.9):
-        self.slope = None
-        self.intercept = None
-        self.lr = learning_rate
-        self.eps = epsilon
-        self.weights = np.ndarray
-    
-    def fit(self, x_train: np.ndarray, y_train: np.ndarray):
-        regression: np.ndarray = np.c_[x_train, np.ones(len(x_train))]
-        self.weights = np.ones(regression.shape[1])
-        # gradient descent
-        loss = 1
-        while(loss > self.eps):
-            y_pred = np.matmul(regression, self.weights.T)
-            partial = np.matmul(regression.T, (y_train - y_pred))
-            loss = np.sum(np.sqrt(np.square(partial)))
-            self.weights = self.weights.T + (self.lr * partial)
-
-            if(np.isnan(loss)):
-                print('Model diverged, try different LR')
-    
-    def predict(self, x_test: np.ndarray):
-        return np.matmul(self.weights[:-1], (x_test.T + self.weights[-1]))
-
 def visualize_simple_regression(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray):
     lr_preds = SimpleLinearRegression()
     lr_preds.fit(x_train, y_train)
@@ -66,16 +34,4 @@ def visualize_simple_regression(x_train: np.ndarray, y_train: np.ndarray, x_test
     plt.plot(x_test, result, color='red', marker='o', label='Simple Linear Regression Line')
     plt.xlabel('X-Test Data')
     plt.ylabel('Prediction Res')
-    plt.show()
-
-def visualize_multi_regression(x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_test: np.ndarray):
-    lr_preds = MultiLinearRegression()
-    lr_preds.fit(x_train, y_train)
-    result = lr_preds.predict(x_test)
-
-    # plot scatter
-    plt.scatter(y_test, result, label='Value')
-    # plt.plot(x_test, result, color='red', marker='o', label='Multi Linear Regression Line')
-    plt.xlabel('Test Data')
-    plt.ylabel('Prediction Data')
     plt.show()
--- section3/run_section3.py
+++ section3/run_section3.py
@@ -8,14 +8,13 @@ from tools.classification_metrics import evaluation_report
 from decision_tree import DecisionTreeClassifier, visualize_gini_vs_entropy_tree
 from knn import kNN, visualize_knn_best_k
 from naive_bayes import GaussianNaiveBayes
-from regression import SimpleLinearRegression, MultiLinearRegression, visualize_simple_regression, visualize_multi_regression
+from regression import SimpleLinearRegression, visualize_simple_regression
 from tools.utils import load_csv_data, train_test_split
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Section 3 Argument Parser')
-    parser.add_argument('-m', '--mode', help='Choose mode for running (predict, vis, compare)', required=True, type=str)
-    parser.add_argument('-d', '--dataset', help='Define Dataset Path', required=True, type=str)
-    parser.add_argument('-a', '--algo', help='Run specific algo (simple_regression, multi_regression, knn, naive, tree)', type=str)
+    parser.add_argument('-m', '--mode', help='Choose mode for running (predict, vis)', required=True, type=str)
+    parser.add_argument('-a', '--algo', help='Run specific algo (regression, knn, naive, tree)', type=str)
     parser.add_argument('-ts', '--train_split', help='Define training percentage over testing percentage', default=0.7, type=float)
 
     # linear regression specific
@@ -38,30 +37,25 @@ if __name__ == '__main__':
     #define dataset
     # You can always change which columns you want to run (choose at least 2 columns with 1 column as y value)
     # Keep in mind that simple linear regression only use one feature to process
-    if args.algo != 'simple_regression':
+    dataset_path = './dataset/whr_dataset.csv'
+    if args.algo != 'regression':
         x_columns = ['Family','Health (Life Expectancy)','Economy (GDP per Capita)','Freedom','Trust (Government Corruption)','Generosity']
     else:
-        x_columns = ['Freedom'] 
+        x_columns = ['Generosity'] 
     y_columns = 'Region'
-    x_data, y_data = load_csv_data(args.dataset, x_columns, y_columns)
+    x_data, y_data = load_csv_data(dataset_path, x_columns, y_columns)
     x_train, y_train, x_test, y_test = train_test_split(x_data, y_data, args.train_split)
     
     # choose prediction
     if args.mode == 'predict':
-        if args.algo == 'simple_regression': # for Simple Linear Regression algorithm
+        if args.algo == 'regression': # for Simple Linear Regression algorithm
             lr_preds = SimpleLinearRegression()
             lr_preds.fit(x_train, y_train)
             result = lr_preds.predict(x_test)
-        elif args.algo == 'multi_regression': # for Multi Linear Regression algorithm
-            mlr_preds = MultiLinearRegression(args.learning_rate, args.epsilon)
-            mlr_preds.fit(x_train, y_train)
-            result = mlr_preds.predict(x_test)
         elif args.algo == 'knn': # for K-Nearest Neighbor algorithm
-            knn_preds = kNN(args.k_neighbours, args.dist_metric)
+            knn_preds = kNN(args.k_neighbours, args.dist_metric, args.p_value)
             knn_preds.fit(x_train, y_train)
             result = knn_preds.predict(x_test)
-            result_proba = knn_preds.predict_proba(x_test)
-            print(result_proba)
         elif args.algo == 'naive': # for Naïve Bayes algorithm
             naive_preds = GaussianNaiveBayes()
             naive_preds.fit(x_train, y_train)
@@ -73,10 +67,8 @@ if __name__ == '__main__':
         evaluation_report(args.algo, result, y_test)
     # choose visualize
     elif args.mode == 'vis':
-        if args.algo == 'simple_regression':
+        if args.algo == 'regression':
             visualize_simple_regression(x_train, y_train, x_test, y_test)
-        elif args.algo == 'multi_regression':
-            visualize_multi_regression(x_train, y_train, x_test, y_test)
         elif args.algo == 'knn':
             visualize_knn_best_k(x_train, y_train, x_test, y_test, args.dist_metric, args.p_value)
         elif args.algo == 'tree':
