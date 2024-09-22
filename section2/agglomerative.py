df26dc58690eabae147856cbec4a379632c1a08f
rogcomfox <joeg.11021996@gmail.com>
2024-09-22 21:05:38 +0700

    fix: restructure agglomerative clustering

--- section2/agglomerative.py
+++ section2/agglomerative.py
@@ -1,41 +1,85 @@
 import numpy as np
 import os
-import pandas as pd
 import sys
-
+from sklearn.metrics.pairwise import euclidean_distances
 # enable import from other directory
 sys.path.insert(0, os.getcwd())
 
-from tools.utils import calc_distance
+from tools.utils import calc_distance, calc_linkage
 
 class AgglomerativeClustering:
-    def __init__(self, n_clusters: int = 4, dist_metric: str = 'euclid', p: int = 3):
+    '''
+        Class of Hierarchical Clustering using Agglomerative Clustering or bottom-up approach.
+    '''
+    def __init__(self, n_clusters: int = 4, linkage_type: str = 'complete', dist_metric: str = 'euclid', p: int = 3):
         self.n_clusters = n_clusters
+        self.linkage_type = linkage_type
         self.dist_metric = dist_metric
         self.p = p
     
-    def fit_predict(self, x_data: np.ndarray) -> np.array:
-        self.labels = np.arange(len(x_data))
-        self.centroids = [[x] for x in x_data]
-        print(len(self.centroids))
-        for _ in range(self.n_clusters):
-            min_dist = np.inf
-            min_dist_idx = []
+    def argmin(self, cluster_distance: np.ndarray):
+        min_x, min_y = (0,0)
+        min_val = 1e6
+        for i in range(cluster_distance.shape[0]):
+            for j in range(cluster_distance.shape[0]):
+                if j != i:
+                    if cluster_distance[i, j] < min_val:
+                        min_val = cluster_distance[i, j]
+                        min_x = i
+                        min_y = j
+        return min_val, min_x, min_y
 
-            for i in range(len(self.centroids) - 1):
-                for j in range(i + 1, len(self.centroids)):
-                    dist = self.cluster_distance(self.centroids[i], self.centroids[j])
-                    if dist < min_dist:
-                        min_dist = dist
-                        min_dist_idx = (i,j)
-            self.labels[np.equal(self.labels, min_dist_idx[1])] = min_dist_idx[0]
-            self.labels[np.greater(self.labels, min_dist_idx[1])] -= 1
-            self.centroids[min_dist_idx[0]].extend(self.centroids.pop(min_dist_idx[1]))
-        return np.array(self.labels)
+    def calc_cluster_distance(self, member_clusters: dict, x_data: np.ndarray) -> np.ndarray:
+        num_cluster = len(member_clusters)
+        key_cluster = list(member_clusters.keys())
+        final_distance = np.zeros((num_cluster, num_cluster))
+        for i in range(num_cluster):
+            # pick ith cluster
+            i_element = member_clusters[key_cluster[i]]
+            # compare to other cluster
+            for j in range(num_cluster):
+                j_element = member_clusters[key_cluster[j]]
+                dist = euclidean_distances(x_data[i_element], x_data[j_element])
+                dist_linkage = calc_linkage(self.linkage_type, dist)
+                final_distance[i,j] = dist_linkage
+        return final_distance
     
-    def cluster_distance(self, cluster_1: list, cluster_2: list):
-        cluster_1_mean = np.mean(cluster_1, axis=0)
-        cluster_2_mean = np.mean(cluster_2, axis=0)
-        calc_dist = calc_distance(cluster_1_mean, cluster_2_mean, self.dist_metric, self.p)
-        return (len(cluster_1) * len(cluster_2)) / (len(cluster_1) + len(cluster_2)) * calc_dist
+    def fit(self, x_data: np.ndarray) -> np.ndarray:
+        n_samples, _ = x_data.shape
+        member_clusters = {i: [i,] for i in range(n_samples)}
+        temp_labels = np.zeros((n_samples - 1, 4))
 
+        for i in range(n_samples - 1):
+            # num_cluster = len(member_clusters)
+            key_cluster = list(member_clusters.keys())
+            cluster_distance = self.calc_cluster_distance(member_clusters, x_data)
+            _, temp_min_x, temp_min_y = self.argmin(cluster_distance)
+            temp_x = key_cluster[temp_min_x]
+            temp_y = key_cluster[temp_min_y]
+            # update labels
+            temp_labels[i, 0] = temp_x
+            temp_labels[i, 1] = temp_y
+            temp_labels[i, 2] = cluster_distance[temp_min_x, temp_min_y]
+            temp_labels[i, 3] = len(member_clusters[temp_x]) + len(member_clusters[temp_y])
+            # merge cluster
+            member_clusters[i + n_samples] = member_clusters[temp_x] + member_clusters[temp_y]
+            del member_clusters[temp_x]
+            del member_clusters[temp_y]
+        self.final_labels = temp_labels
+        return self.final_labels
+
+    def predict(self, x_data: np.ndarray):
+        n_samples, _ = x_data.shape
+        preds = np.zeros((n_samples))
+        cluster_members = dict([(i,[i]) for i in range(n_samples)])
+        for i in range(n_samples - self.n_clusters):
+            x = int(self.final_labels[i,0])
+            y = int(self.final_labels[i,1])
+            cluster_members[n_samples + i] = cluster_members[x] + cluster_members[y]
+            del cluster_members[x]
+            del cluster_members[y]
+        keys = list(cluster_members.keys())
+        for i in range(len(keys)):
+            samples_in_cluster = cluster_members[keys[i]]
+            preds[samples_in_cluster] = i
+        return np.array(preds, dtype=np.int64)
--- section2/run_section2.py
+++ section2/run_section2.py
@@ -13,13 +13,15 @@ from tools.clustering_metrics import evaluation_report
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Section 2 Argument Parser')
     parser.add_argument('-m', '--mode', help='Choose mode for running (predict, vis, compare)', required=True, type=str)
-    parser.add_argument('-d', '--dataset', help='Define Dataset Path', required=True, type=str)
     parser.add_argument('-a', '--algo', help='Run specific algo (kmeans, agglo)', type=str)
     parser.add_argument('-ts', '--train_split', help='Define training percentage over testing percentage', default=0.7, type=float)
     
     # kmeans args
-    parser.add_argument('-nc', '--n_cluster', help='Define number of cluster for kmeans', default=8, type=int)
-    parser.add_argument('-it', '--iter', help='Define number of iteration of training in Kmeans', default=100, type=int)
+    parser.add_argument('-nc', '--n_cluster', help='Define number of cluster for kmeans', default=2, type=int)
+    parser.add_argument('-it', '--iter', help='Define number of iteration of training in Kmeans', default=300, type=int)
+
+    # agglomerative args
+    parser.add_argument('-li', '--linkage', help='Define Linkage type (complete, single)', default='complete', type=str)
 
     # misc args
     parser.add_argument('-dm', '--dist_metric', help='Distance metric used (euclid, manhattan, minkowski)', type=str, default='euclid')
@@ -28,20 +30,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # define dataset first (We use Shop Customer Dataset for Clustering Learning)
+    dataset_path = './dataset/shop_customer_dataset.csv'
     x_columns = ['Annual Income ($)','Spending Score (1-100)','Work Experience','Family Size']
     y_columns = 'Gender'
-    x_data, y_data = load_csv_data(args.dataset, x_columns, y_columns)
+    x_data, y_data = load_csv_data(dataset_path, x_columns, y_columns)
     x_train, y_train, x_test, y_test = train_test_split(x_data, y_data, args.train_split)
 
     if args.mode == 'predict':
         if args.algo == 'kmeans':
-            kmeans_pred = KMeans(args.n_cluster, args.iter, args.dist_metric)
+            kmeans_pred = KMeans(args.n_cluster, args.iter)
             centroid, inertia = kmeans_pred.fit(x_data)
             result = kmeans_pred.predict(x_test)
             evaluation_report(x_test, result, args.dist_metric)
         elif args.algo == 'agglo':
-            agglo_pred = AgglomerativeClustering(args.n_cluster)
-            result = agglo_pred.fit_predict(x_data)
+            # we limit the data only for agglomerative clustering due its slower compared to kmeans
+            # TODO: distance still used sklearn approach, need to change to handcrafted only for agglomerative
+            x_data = x_data[:40, :]
+            agglo_pred = AgglomerativeClustering(args.n_cluster, args.linkage, args.dist_metric)
+            labels = agglo_pred.fit(x_data)
+            result = agglo_pred.predict(x_data)
             evaluation_report(x_data, result, args.dist_metric)
     elif args.mode == 'vis':
         if args.algo == 'kmeans':
