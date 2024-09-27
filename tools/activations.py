fix: update activations formula

tools/activations.py
@@ -17,12 +17,17 @@ def choose_activation(x_data: np.ndarray, activation: str, state: str):
             result = tanh(x_data)
         else:
             result = tanh_derivative(x_data)
+    elif activation == 'softmax':
+        if state == 'forward':
+            result = softmax(x_data)
+        else:
+            result = softmax_derivative(x_data)
     return result
 
 # softmax (forward pass and its backward pass)
-def softmax(x_data: np.ndarray) -> np.ndarray:
+def softmax(x_data: np.ndarray, axis: int = -1) -> np.ndarray:
     e_x = np.exp(x_data - np.max(x_data))
-    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+    return e_x / np.sum(e_x, axis=axis, keepdims=True)
 
 def softmax_derivative(x_data: np.ndarray) -> np.ndarray:
     return softmax(x_data) * (1 - softmax(x_data))
@@ -36,14 +41,14 @@ def sigmoid_derivative(x_data: np.ndarray) -> np.ndarray: # gradient calc in sig
 
 # relu (forward pass and its backward pass)
 def relu(x_data: np.ndarray) -> np.ndarray:
-    return np.where(x_data >= 0, x_data, 0)
+    return np.maximum(0, x_data)
 
 def relu_derivative(x_data: np.ndarray) -> np.ndarray: # gradient calc in relu
-    return np.where(x_data >= 0, 1, 0)
+    return 1. * (x_data > 0)
 
 # tanh (forward pass and its backward pass)
 def tanh(x_data: np.ndarray) -> np.ndarray:
-    return 2 / (1 + np.exp(-2 * x_data)) - 1
+    return np.tanh(x_data)
 
 def tanh_derivative(x_data: np.ndarray) -> np.ndarray:
     return 1 - np.power(tanh(x_data), 2)