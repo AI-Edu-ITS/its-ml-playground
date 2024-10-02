fix: bce loss, prevent overflow

tools/activations.py
@@ -34,10 +34,12 @@ def softmax_derivative(x_data: np.ndarray) -> np.ndarray:
 
 # sigmoid (forward pass and its backward pass)
 def sigmoid(x_data: np.ndarray) -> np.ndarray:
+    # supress overflow warning in exponent computation
+    x_data = np.clip(x_data, -709.78, 709.78)
     return 1. / (1. + np.exp(-x_data * 1.0))
 
 def sigmoid_derivative(x_data: np.ndarray) -> np.ndarray: # gradient calc in sigmoid
-    return x_data * (1 - x_data)
+    return sigmoid(x_data) * (1. - sigmoid(x_data))
 
 # relu (forward pass and its backward pass)
 def relu(x_data: np.ndarray) -> np.ndarray:

tools/loss.py
@@ -5,9 +5,12 @@ def binary_cross_entropy_loss(preds: np.ndarray, y_data: np.ndarray, eps: float 
     '''
         For only non exclusive class or maximum 2 class only loss (for example class 0 and 1 or class True and False)
     '''
-    y1 = np.sum((-np.log(preds + eps) * y_data))
-    y2 = (-np.log(1 - preds + eps)) * (1 - y_data)
-    return np.mean(y1 + y2)
+    preds = preds.reshape(-1,1)
+    y_data = y_data.reshape(-1,1)
+    preds = np.clip(preds, eps, 1 - eps)
+    y1 = (1 - y_data) * np.log(1 - preds + eps)
+    y2 = y_data * np.log(preds + eps)
+    return -np.mean(y1 + y2, axis=0)
 
 def categorical_cross_entropy_loss(preds: np.ndarray, y_test: np.ndarray) -> float:
     '''